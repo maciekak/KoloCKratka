487069685c02e86cf28d4b3c44cf21ef26cc5d73
Maciej Klimowski <maciek.klimowski44@gmail.com>
2018-12-10 16:56:54 +0100

    Homework and collections example added

---
+++ Zajecia6/CollectionsExample/CollectionsExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sposob dodawania elementów - normalny i nienormalny
+            //Dać metodę wypisującą
+            //Pokazać różne interfejsy
+            //Pokazać array, List, dictionary, linkedlist
+            //Pokazać filtrowanie (Program) - where%, where< i select*
+            //Pokazać LINQ
+            //Pokazać że jest leniwe
+            //Dodać logowanie konsolowe
+            //Zmienić referencje
+            var a = (IEnumerable<int>) (new List<int> {1, 2});
+            Console.ReadKey();
+        }
+    }
+}
